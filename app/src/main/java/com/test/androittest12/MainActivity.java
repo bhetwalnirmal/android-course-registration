819d34efbf6e55bb85a723b4b5c753b23b993564
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-04 10:11:09 -0400

    added total hours and fees

--- app/src/main/java/com/test/androittest12/MainActivity.java
+++ app/src/main/java/com/test/androittest12/MainActivity.java
@@ -3,8 +3,13 @@ package com.test.androittest12;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.test.androittest12.models.Course;
 import com.test.androittest12.models.CourseChoices;
@@ -26,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
     // layout variables
     Spinner spCourseChoices, spCoursesMenu;
+    Button addCourse;
+    TextView tvTotalFee, tvTotalHours;
+    double totalFee;
+    int totalHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         fillData();
         spCourseChoices = (Spinner) findViewById(R.id.spCourses);
         spCoursesMenu = (Spinner) findViewById(R.id.spCoursesMenu);
+        addCourse = (Button) findViewById(R.id.btnAddCourse);
+        tvTotalHours = (TextView) findViewById(R.id.tvTotalHours);
+        tvTotalFee = (TextView) findViewById(R.id.tvTotalFees);
 
         // initializing the course choice spinner
         ArrayAdapter<String> courseChoiceAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, coursesChoiceString);
@@ -42,6 +54,38 @@ public class MainActivity extends AppCompatActivity {
         // initializing the course choice menu spinner
         ArrayAdapter<String> courseMenuAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, coursesString);
         spCoursesMenu.setAdapter(courseMenuAdapter);
+
+        addCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String courseName = (String) spCoursesMenu.getSelectedItem();
+                String courseChoice = (String) spCourseChoices.getSelectedItem();
+
+                Course course = getCourseByName(courseName);
+                boolean isAlreadySelected = false;
+
+                for (Course myCourse : myCourses) {
+                    if (myCourse.getName().equals(courseName)) {
+                        isAlreadySelected = true;
+                        break;
+                    }
+                }
+
+                boolean isValid = checkCourseHours(courseChoice);
+
+                if (!isValid) {
+                    Toast.makeText(MainActivity.this, "You cannot select this course.", Toast.LENGTH_SHORT).show();
+                }
+
+                if (!isAlreadySelected) {
+                    myCourses.add(course);
+                } else {
+                    Toast.makeText(MainActivity.this, "Course is already added to the course list", Toast.LENGTH_SHORT).show();
+                }
+
+                updateTotal();
+            }
+        });
     }
 
     protected void fillData () {
@@ -61,5 +105,54 @@ public class MainActivity extends AppCompatActivity {
         for (CourseChoices courseChoice : courseChoices) {
             coursesChoiceString.add(courseChoice.getName());
         }
+    }
+
+    protected Course getCourseByName (String name) {
+        for (Course course : courses) {
+            if (course.getName() == name) {
+                return course;
+            }
+        }
+
+        return null;
+    }
+
+    protected void updateTotal () {
+        double totalFee = 0;
+        double totalHours = 0;
+
+        for (Course myCourse : myCourses) {
+            totalFee += myCourse.getFee();
+            totalHours += myCourse.getHours();
+        }
+
+        tvTotalFee.setText("$ " + totalFee);
+        tvTotalHours.setText("" + totalHours + " Hours");
+    }
+
+    protected boolean checkCourseHours (String courseChoice) {
+        CourseChoices myChoice = null;
+        int allowedHours = 0;
+
+        for (CourseChoices choice : courseChoices) {
+            if (choice.getName().equals(courseChoice)) {
+                myChoice = choice;
+            }
+        }
+
+        if (myChoice != null) {
+            if (myChoice.getType() == 1) {
+                allowedHours = 21;
+            } else {
+                allowedHours = 19;
+            }
+        }
+
+        int totalHours = 0;
+        for (Course myCourse : myCourses) {
+            totalHours += myCourse.getHours();
+        }
+
+        return allowedHours > totalHours ? true : false;
     }
 }
