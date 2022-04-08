e25a29a31bdf011c60bf92e239cda665071eb486
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-08 11:55:49 -0400

    implement delete button

--- app/src/main/java/com/test/androittest12/SelectedCourseListAdapter.java
+++ app/src/main/java/com/test/androittest12/SelectedCourseListAdapter.java
@@ -1,7 +1,10 @@
 package com.test.androittest12;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.Image;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +17,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class SelectedCourseListAdapter extends BaseAdapter {
     LayoutInflater inflater;
+    Context context;
 
     public SelectedCourseListAdapter(Context context) {
         inflater = LayoutInflater.from(context);
+        this.context = context;
     }
 
     @Override
@@ -42,16 +47,34 @@ public class SelectedCourseListAdapter extends BaseAdapter {
             view = inflater.inflate(R.layout.selected_course_row, null);
             viewHolder = new ViewHolder();
             viewHolder.courseName = (TextView) view.findViewById(R.id.courseName);
-            viewHolder.courseHours = (TextView) view.findViewById(R.id.courseHours);
+            viewHolder.courseHours = (TextView) view.findViewById(R.id.courseHour);
             viewHolder.coursePrice = (TextView) view.findViewById(R.id.coursePrice);
             viewHolder.remove = (ImageView) view.findViewById(R.id.remove);
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
 
+        viewHolder.remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Do you want to remove the course ?");
+                builder.setTitle("warning");
+                builder.setCancelable(true);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.myCourses.remove(i);
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.show();
+            }
+        });
+
         viewHolder.courseName.setText(MainActivity.myCourses.get(i).getName());
         viewHolder.coursePrice.setText("" + MainActivity.myCourses.get(i).getFee());
-//        viewHolder.courseHours.setText(MainActivity.myCourses.get(i).getHours());
+        viewHolder.courseHours.setText("" + MainActivity.myCourses.get(i).getHours());
 
         return view;
     }
